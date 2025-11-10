657504e2b7b1067e0bf309f3294331dcee43b343
Sanjit Kumar Sahoo <sriyanssanjit@gmail.com>
2025-11-10 14:25:03 +0530

    Update welcome message in Flask app

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-   return "Hello from Sanjit's OpenShift Flask App!"
+   return "Hello from Sanjit's welcom to openshift world!"
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8080)
